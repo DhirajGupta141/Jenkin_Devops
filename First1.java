b4d478467220d8fc02fdc23a3511d36de4a17f10
Dhiraj Gupta <62639582+DhirajGupta141@users.noreply.github.com>
2022-03-20 21:52:54 +0530

    Update First1.java

--- First1.java
+++ First1.java
@@ -1,7 +1,7 @@
 public class First1
 {
     public static void main(String[] args) {
-        for(int i=0;i<10;i++)
-            System.out.println("Tic Tic  "+ i+1+ " !"); 
+        for(int i=1;i<=10;i++)
+            System.out.println("Tic Tic  "+ i + " !"); 
     }
 }
